2f154cd4b579a7276d48eb5c7e5b9ce8c16e86dd
decyphertek-io <ciphertek@protonmail.com>
2023-06-25 16:12:17 -0500

    decyphertek-io

--- startup.sh
+++ startup.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
+while :
+do
+
 # ASCII art header 
-echo """ 
+    echo """ 
  ____                        _               _       _      _       
 |  _ \  ___  ___ _   _ _ __ | |__   ___ _ __| |_ ___| | __ (_) ___  
 | | | |/ _ \/ __| | | | '_ \| '_ \ / _ \ '__| __/ _ \ |/ / | |/ _ \ 
 | |_| |  __/ (__| |_| | |_) | | | |  __/ |  | ||  __/   < _| | (_) |
 |____/ \___|\___|\__, | .__/|_| |_|\___|_|   \__\___|_|\_(_)_|\___/ 
                  |___/|_|                                           
-"""
+    """
 
-# Instructions
-echo "Welcome to Decyphertek - Decoding Technology."  
-echo "---------------------------------------------"
-# Choose your docker compose server. 
-ls
-echo "----------------------------------------------"
-echo "Which docker compose would you like to run?"
-read DOCKER_CHOICE
-# Depending on the choice will run a docker-compose.yml for the chosen server.  
-echo "We are launching $DOCKER_CHOICE, please be patient."
-cd $DOCKER_CHOICE
-docker-compose up -d 
-docker ps | grep $DOCKER_CHOICE
-echo "-------------------------------"
-echo "$DOCKER_CHOICE is now ready!!!"
-echo "-------------------------------"
+    # Instructions
+    echo "Welcome to Decyphertek - Decoding Technology."  
+    echo "---------------------------------------------"
+    # Choose your docker compose server. 
+    ls
+    echo "----------------------------------------------"
+    echo "Which docker compose would you like to run?"
+    read DOCKER_CHOICE
+    # Depending on the choice will run a docker-compose.yml for the chosen server.  
+    echo "We are launching $DOCKER_CHOICE, please be patient."
+    cd $DOCKER_CHOICE
+    docker-compose up -d 
+    docker ps | grep $DOCKER_CHOICE
+    echo "-------------------------------"
+    echo "$DOCKER_CHOICE is now ready!!!"
+    echo "-------------------------------"
+	echo "Press CTRL+C to exit"
+	sleep 3
+done
